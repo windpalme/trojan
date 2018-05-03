958cd21d1a7fb3050eb310ac8bedbb49db0d5aaf
windpalme <windpalme@gmail.com>
2018-05-04 01:12:29 +0800

    Update toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -2,9 +2,12 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i486-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER i486-mingw32-g++)
-SET(CMAKE_RC_COMPILER i486-mingw32-windres)
+set(GNU_HOST x86_64-w64-mingw32)
+set(COMPILER_PREFIX "${GNU_HOST}-")
+
+set(CMAKE_C_COMPILER ${COMPILER_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}g++)
+set(CMAKE_RC_COMPILER ${COMPILER_PREFIX}windres)
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH /usr/i486-mingw32)
